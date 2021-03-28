a4f615d2b1f5f040d162e1f59753cfbebc9bb171
UvJngGT19oX <49621424+JuanMarchant@users.noreply.github.com>
2021-03-28 01:54:41 -0300

    Create Condicionales.js

---
+++ Condicionales.js
@@ -0,0 +1,27 @@
+/* ------------------------------------------------------------ */
+var num = 15;
+if (num >= 10) {
+    console.log("el número es mayor que 10");
+} // se escribira "el numero es mayor que 10"
+
+/* ------------------------------------------------------------ */
+
+var name = "Bruce";
+if (name != "Bruce") {
+    console.log("¿Cuál es tu nombre?");
+} // como la variable name es = a bruce pasa a else
+else {
+    console.log("Hola, Bruce!");
+} // entonces se se ejecuta y se escribe "Hola, Bruce!"
+
+/* ------------------------------------------------------------ */
+
+var num1 = 10;
+var num2 = 15;
+if (num1 <= num2) {
+    num1 = num1 + 10;
+} // if es verdadero entonces se ejecuta y la variable num1 pasa a ser 20, num1 = 20
+console.log(num1); // luego se escribe "20"
+console.log(num2); // y por ultimo "15"
+
+/* ------------------------------------------------------------ */
