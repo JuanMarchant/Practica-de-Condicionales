e67c763e68b922f8646ae7faac0f777620ec6121
UvJngGT19oX <49621424+JuanMarchant@users.noreply.github.com>
2021-03-28 23:00:53 -0300

    Update Desafios.js

--- Desafios.js
+++ Desafios.js
@@ -13,3 +13,23 @@ if(x > 1){
 imprimeHasta(1000000); // debe imprimir todos los enteros desde el 1 hasta el 1000000
 y = imprimeHasta(-10); 
 console.log(y);
+
+
+
+/*--------------------------------------*/
+
+
+function printSum(x) {
+    var sum = 0;
+    for(var i = 0; i < x + 1; i++){
+        sum += i;
+        console.log(sum);
+}
+    return sum
+}
+y = printSum(255) // debe imprimir todos los enteros desde el o hasta el 255, y la suma parcial en cada punto
+console.log(y) // debe imprimir 32640
+
+
+
+/*--------------------------------------*/
